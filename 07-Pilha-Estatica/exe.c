b15f81d76bb2e16904f0cb441575023f407cef94
Vinicius Soares <vsoares446@gmail.com>
2023-03-30 08:41:21 -0300

    Pilha Estática

---
+++ 07-Pilha-Estatica/exe.c
@@ -0,0 +1,18 @@
+
+# include <stdio.h>
+
+int multiplica_tres(int x, int y, int z)
+{
+    return multiplica_dois(x, y) * z;
+}
+
+int multiplica_dois(int a, int b)
+{
+    return a * b;
+}
+
+int main()
+{
+    printf("%d * %d * %d = %d", 4, 5, 3, multiplica_tres(5, 4, 3));
+    return 0;
+}
---
+++ HackerRank/ent/05-Ponteiro-em-structs-batalha-em-turnos-v1.1.c
@@ -0,0 +1,190 @@
+/*Após ter o protótipo de jogo de RPG aprovado pelo CEO de uma 
+importante empresa de games, Gladstone decide fazer melhorias 
+na jogabilidade. Dessa vez o jogo deverá ter mais recursos. 
+Consequentemente, a estrutura de cada personagem deve ter os 
+seguintes campos: nome, energia, defesa, força e três golpes 
+diferentes (A1, A2, A3), sendo cada um com um valor diferente. 
+Após a execução de um golpe, o dano é dado pela seguinte equação:
+
+Caso o valor da equação acima for negativo, o dano será de 1 ponto 
+apenas. Em cada batalha, o perdedor será o personagem que tiver a 
+energia zerada ou que tiver o menor valor de energia. Também, poderá 
+ocorrer empate. É importante ressaltar que o personagem com energia 
+zerada não pode mais atacar!
+
+O jogo também tem mais novidades novidades: o personagem, em vez de 
+atacar, pode tomar uma poção de cura, que restaura 200 pontos; quem 
+inicia o ataque não é necessariamente o primeiro personagem; o valor 
+máximo de energia é 1000 (personagem completamente saudável).
+
+Implemente funções para o gerenciamento de batalha entre dois personagens.
+Para isso, devem ser utilizados ponteiros de structs. Exemplo de protótipos:
+
+void reduzir_energia(Tipo *p, int pontos_dano); // reduz pontos_dano 
+pontos na saúde do personagem (valor mínimo de saúde: 0)
+
+void restaurar(Tipo *p); // aumenta 200 pontos na saúde do 
+personagem (valor máximo de saúde: 1000)
+
+Input Format
+Nas duas primeiras linhas, devem ler lidos os dados dos personagens. 
+Cada uma das duas primeiras linhas tem a seguinte seguência: "nome (
+vetor de char) energia (int) defesa (int), força (int), ataque 1 
+(int), ataque 2 (int), ataque 3 (int)""
+
+Na linha seguinte, deve ser lida a quantidade de ações (N) que deverá ter a batalha.
+
+A leitura das próximas N linhas devem ser no formato: "P A" (o personagem toma 
+uma poção de cura) ou P A G (o personagem realiza um ataque), onde:
+
+P: do tipo inteiro, representa o personagem que executa a ação. O valor pode 
+ser 1 (primeiro persongem lido) ou 2
+
+A: é um caractere que representa uma ação, que pode ser "C" (cura) ou "A" ataque.
+
+Caso a ação representada pelo caractere "A", ainda deverá ser definido qual 
+dos 3 três golpes possíveis deve ser executado, ou seja, o valor deve ser 1, 2 ou 3
+
+Constraints
+.
+Output Format
+
+Na primeira e segunda linha, imprimir o nome e a energia restante do primeiro e 
+do segundo personagem, após a execução das rodadas de ataques.
+
+Na terceira lunha, imprimir o nome do personagem vencedor ou "empate", caso os 
+personagens tiverem a mesma quantidade de energia (neste exercício não levando 
+em conta que os personagens possam iniciar uma batalha com diferentes valores 
+de energia).
+
+Sample Input 0
+idril 1000 200 150 10 20 30
+gidal 1000 100 300 20 25 15
+8
+1 A 3
+2 A 2
+1 A 2
+2 A 1
+1 A 1
+2 A 3
+1 C
+2 A 3
+
+Sample Output 0
+idril 725
+gidal 790
+gidal
+
+Sample Input 1
+jilian 1000 150 50 50 30 40
+endric 1000 150 50 35 50 40
+2
+1 C
+2 C
+
+Sample Output 1
+jilian 1000
+endric 1000
+empate*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define dim 2
+
+typedef struct personagem
+{
+    char nome[100];
+    int energia;
+    int defesa;
+    int forca;
+    int A;
+    int golpe; 
+}Personagem;
+
+//Caulcula o dano de cada ataque
+void atacar(Personagem *atacante, Personagem *atacado) 
+{
+    int dano = atacante->forca + atacante->golpe - atacado->defesa;
+    if (dano <= 0) 
+    {
+        dano = 1;
+    }
+    atacado->energia -= dano;
+    if (atacado->energia < 0) 
+    {
+        atacado->energia = 0;
+    }
+}
+
+//Funcao batalha chama a funcao ataque a cada rodada e mostra o vencedor
+void batalha(Personagem *p1, Personagem *p2, int turnos) 
+{
+    int i,j;
+    char opcao;
+    Personagem vetor_g[3];
+    for (i = 0; i < turnos; i++) 
+    {
+        printf("1 ");
+        scanf("%c",&opcao);
+
+        
+        atacar(p1, p2);
+        
+        if (p2->energia<=0)
+        {
+            break;
+        }
+        
+        atacar(p2, p1);
+        
+        if (p1->energia == 0 || p2->energia == 0) 
+        {
+            break;
+        }
+    }
+    if (p1->energia == p2->energia) 
+    {
+        printf("%s %d\n", p1->nome, p1->energia);
+        printf("%s %d\n", p2->nome, p2->energia);
+        printf("empate\n");
+    } 
+    else if (p1->energia > p2->energia) 
+    {
+        printf("%s %d\n", p1->nome, p1->energia);
+        printf("%s %d\n", p2->nome, p2->energia);
+        printf("%s\n", p1->nome);
+    } 
+    else 
+    {
+        printf("%s %d\n", p1->nome, p1->energia);
+        printf("%s %d\n", p2->nome, p2->energia);
+        printf("%s\n", p2->nome);
+    }
+
+}
+
+int main(void)
+{
+    int turnos,i;
+
+    Personagem vetor_p[dim],golpe[3];
+    
+    //Entrada de dados dos personagens
+    for ( i = 0; i < dim; i++)
+    {
+        scanf("%s %d %d %d",vetor_p[i].nome,&vetor_p[i].energia,&vetor_p[i].defesa,&vetor_p[i].forca);
+        for ( i = 0; i < 3; i++)
+        {
+            scanf("%d",&golpe[i].A);
+        }
+        
+    }
+    scanf("%d", &turnos);
+    batalha(&vetor_p[0], &vetor_p[1],turnos);
+
+    return 0;
+
+}
