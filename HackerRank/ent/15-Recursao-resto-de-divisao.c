247e0c67da48cb25af6f71386530817854ecc40b
Vinicius Soares <62343620+vncs-srs@users.noreply.github.com>
2023-05-10 16:36:56 -0300

    Criação: 15 Recursão.

---
+++ HackerRank/ent/15-Recursao-resto-de-divisao.c
@@ -0,0 +1,62 @@
+/*Implemente uma função recursiva que calcule o resto da divisão entre dois números inteiros.
+
+Input Format
+Na primeira linha deve ser lido um número inteiro N referente a quantidade de leituras de entradas.
+Para cada uma das N próximas linhas, ler dois números inteiros (a e b)
+
+Constraints
+.
+Output Format
+
+Em cada uma das N linhas, imprimir o resultado do resto da divisão dos respectivos números inteiros.
+
+Sample Input 0
+5
+1 2
+6 7
+8 6
+0 20
+23 6
+
+Sample Output 0
+1
+6
+2
+0
+5*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int resto_rec(int numerodor,int divisor)
+{
+    return numerodor%divisor;
+}
+void imprimir(int vetor[],int n)
+{
+    int i;
+    for ( i = 0; i < n; i++)
+    {
+        printf("%d \n",vetor[i]);
+    }
+    
+}
+
+
+int main(void)
+{
+    int i,n,numerador,divisor;
+
+    scanf("%d",&n);
+    int vetor[n];
+    for ( i = 0; i < n; i++)
+    {
+        scanf("%d %d",&numerador,&divisor);
+        vetor[i]=resto_rec(numerador,divisor);
+    }
+    imprimir(vetor,n);
+    
+    return 0;
+}
