fc4c78038d3d8c0df84aed57f709b42edccbfe75
Vinicius Soares <62343620+vncs-srs@users.noreply.github.com>
2023-05-12 10:57:18 -0300

    Criação: 16 Algorítimos de busca.

---
+++ HackerRank/ent/16-Algoritmos-de-busca-recuperacao-recorrente-mover-para-frente-recursivo.c
@@ -0,0 +1,34 @@
+/*Implemente uma função que receba um arranjo de números inteiros. Nessa função deverá ser implementado, utilizando recursão, o método de recuperação recorrente mover-para-frente, no qual, cada vez que um registro é recuperado com sucesso, ele é movimentado para a primeira posição.
+
+Input Format
+
+Na primeira linha, ler um número inteiro (n) referente ao tamanho do arranjo.
+
+Na segunda linha, ler os n elementos do vetor
+
+Na terceira linha deve ser lida a quantidade de chaves que deverão ser procuradas). A partir da próxima linha, as chaves que serão procuradas devem ser lidas.
+
+Constraints
+
+.
+
+Output Format
+
+Após execução do algoritmo para cada caso teste, imprimir o arranjo.
+
+Sample Input 0
+
+14
+1 2 3 4 5 6 7 8 9 10 11 12 13 14
+8
+1
+6
+7
+6
+6
+0
+6
+7
+Sample Output 0
+
+7 6 1 2 3 4 5 8 9 10 11 12 13 14*/
